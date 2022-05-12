417c98eee016a66e0d7c1e01f29c329f0e096d9f
iamosl <osl.lavangad@gmail.com>
2022-05-12 14:56:54 +0530

    fixing bug

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,7 +4,7 @@ import './Profile.css'
 import { Button } from '@mui/material';
 import ProfileDialog from '../components/ProfileDialog';
 import { Container } from '@mui/material';
-import { Grid,TextField,Card,CardActions,CardHeader,CardContent,Fab } from '@mui/material';
+import { Grid, TextField, Card, CardActions, CardHeader, CardContent, Fab } from '@mui/material';
 
 const Profile = () => {
     const profile = getLocalStorageData('currentProfile');
@@ -14,54 +14,55 @@ const Profile = () => {
         setOpen(false);
     };
 
-    const handleClickOpen=()=>{
+    const handleClickOpen = () => {
         setOpen(true);
     }
     return (
-        <Container className='Profile' style={{ justifyContent: 'center', height: "100%", width: "100%"}}>
+        <Container className='Profile' style={{ justifyContent: 'center', height: "100%", width: "100%" }}>
             <h1>Your Profile</h1>
-            <Button variant="contained" onClick={handleClickOpen} style={{backgroundColor:"#4863A0",marginLeft:"100%"}}>Edit</Button>
-            <ProfileDialog open={open} handleClose={handleClose}/>
+            <Button variant="contained" onClick={handleClickOpen} style={{ backgroundColor: "#4863A0", marginLeft: "100%" }}>Edit</Button>
+            <ProfileDialog open={open} handleClose={handleClose} />
             {/* <form style={{marginTop:"5%" }}> */}
-                <Grid container spacing={5}>
-                    {
-                        Object.keys(profile).filter(function(item){
-                            return item!='user' && item!='skills' && item!='id';
-                    }).map((field)=>(
+            <Grid container spacing={5}>
+                {
+                    Object.keys(profile).filter(function (item) {
+                        return item != 'user' && item != 'skills' && item != 'id';
+                    }).map((field) => (
                         <Grid item lg={4}>
-                        <TextField
-                        id={field}
-                        label={field.replace(
-                            /\w\S*/g,
-                            function(txt) {
-                              return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()})}
-                        multiline
-                        variant="filled"
-                        defaultValue={profile[field]}
-                        InputProps={{
-                            readOnly: true,
-                        }}
-                        sx={{backgroundColor:"white"}}
-                        />
-                        </Grid>))} 
-                </Grid>
-                <Card sx={{ width: "50%", marginBottom: "20px",marginTop:"10%",backgroundColor:"#F5F5F5" }}>
-                <CardHeader title={"Your Skills"}/>
+                            <TextField
+                                id={field}
+                                label={field.replace(
+                                    /\w\S*/g,
+                                    function (txt) {
+                                        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+                                    })}
+                                multiline
+                                variant="filled"
+                                defaultValue={profile[field]}
+                                InputProps={{
+                                    readOnly: true,
+                                }}
+                                sx={{ backgroundColor: "white" }}
+                            />
+                        </Grid>))}
+            </Grid>
+            <Card sx={{ width: "50%", marginBottom: "20px", marginTop: "10%", backgroundColor: "#F5F5F5" }}>
+                <CardHeader title={"Your Skills"} />
                 <CardContent >
-                <Grid container>
-                    {
-                        profile['skills'].map((skill)=>(
-                            <Grid item key={skill.id}>
-                                <Fab color="secondary" aria-label={skill.id} sx={{fontSize:"15px",marginBottom:"20px",marginInlineEnd:"10px"}} size="medium" variant="extended">
-                                    {skill.skill}
-                                </Fab>
-                            </Grid>
-                        ))
-                    }
-                </Grid>
+                    <Grid container>
+                        {
+                            profile['skills'] != null && profile['skills'].map((skill) => (
+                                <Grid item key={skill.id}>
+                                    <Fab color="secondary" aria-label={skill.id} sx={{ fontSize: "15px", marginBottom: "20px", marginInlineEnd: "10px" }} size="medium" variant="extended">
+                                        {skill.skill}
+                                    </Fab>
+                                </Grid>
+                            ))
+                        }
+                    </Grid>
                 </CardContent>
-                </Card>
-                
+            </Card>
+
             {/* </form> */}
         </Container>
     )
